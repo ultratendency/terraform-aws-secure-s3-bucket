e2c30fc37cc62308e4f39a5c52bcc1d3e4a71acb
Jan Hentschel <jan.hentschel@ultratendency.com>
2023-07-23 18:53:59 +0200

    Add missing aws_mks_key_policy

--- main.tf
+++ main.tf
@@ -110,6 +110,27 @@ resource "aws_kms_alias" "this" {
   target_key_id = aws_kms_key.this.key_id
 }
 
+resource "aws_kms_key_policy" "this" {
+  key_id = aws_kms_key.this.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = aws_s3_bucket.this.bucket
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = [
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+          ]
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
